b789350c474eba60e0f712d435e6b797ce706994
AnnaMagradze <annamagdav@mail.ru>
2023-04-24 15:24:06 +0300

    На данном моменте я создала нумерования  клеток и росчертила поле. Они расчертились на клетки в котрые нужно заполнить числа.

--- game.py
+++ game.py
@@ -3,17 +3,23 @@ def print_field(board, num):
     for i in range (9):
         if i % 3 == 0 :
             print('|', end="")
-        print(board[i//3][i % 3], end='|')
+        if board[i//3][i%3]==' 'and num:
+            print (i+1,end="|")
+        else:
+            print(board[i//3][i % 3], end='|')
         if i % 3 == 2:
             print()
             print('-' * 7)
-
-
+def player(symdol):
+    print_field(board,True)
+    number =int(input(" введите номер ячейки: "))-1
+    while number< 0 or number>8 or board[number//3][number % 3]!=" ":
+        print('ошибка: нельзя записать в данную ячейку!')
+        number = int(input(" введите номер ячейки: ")) - 1
+    board[number // 3][number % 3] = symdol
 board=[
     [' ', ' ', ' '],
     [' ', ' ', ' '],
     [' ', ' ', ' ']
     ]
-
-
-print_field(board, False)
+player("x")
