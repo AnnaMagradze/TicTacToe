09f6c753920e1f54a8c132bc61a21ead9ec2df9b
AnnaMagradze <annamagdav@mail.ru>
2023-04-27 23:18:56 +0300

    На данном моменте я создала нумерования  клеток и росчертила поле. Они расчертились на клетки в котрые нужно заполнить числа. На данном моменте я создала клетки для игры с нумирацией. и предложила пользователю ввести числа. Если пользыватель ввел не правильно, то программа выведет ошибку На данном этапе ёя дакла пользователю выбрать номер яейки. И прописала конец игры.

--- game.py
+++ game.py
@@ -10,6 +10,19 @@ def print_field(board, num):
         if i % 3 == 2:
             print()
             print('-' * 7)
+
+
+def game_continue(board):
+    for i in range(3):
+        if board[0][i] == board[1][i] and board[0][i] == board[2][i] and board[0][i] != ' ':
+            return False
+        if board[i][0] == board[i][1] and board[i][0] == board[i][2] and board[i][0] != ' ':
+            return False
+    if ((board[0][0] == board[1][1] == board[2][2]) or (board[2][0] == board[1][1] == board[0][2])) and board[1][1] != ' ':
+        return False
+    return True
+
+
 def player(symdol):
     print_field(board,True)
     number =int(input(" введите номер ячейки: "))-1
@@ -17,9 +30,15 @@ def player(symdol):
         print('ошибка: нельзя записать в данную ячейку!')
         number = int(input(" введите номер ячейки: ")) - 1
     board[number // 3][number % 3] = symdol
+
+
 board=[
     [' ', ' ', ' '],
     [' ', ' ', ' '],
     [' ', ' ', ' ']
     ]
-player("x")
+symbols = ['x', 'o']
+counter= 0
+while game_continue(board):
+    player(symbols[ counter% 2])
+print_field(board,False)
